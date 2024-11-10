4ccd3c6169c51f3f92b46d5a76b2179f011f641b
RustedSwords <manavmakwana27@gmail.com>
2024-11-10 12:33:25 +0100

    fixed a user error

--- engine/dataset.py
+++ engine/dataset.py
@@ -2,11 +2,13 @@ from torch.utils.data import Dataset
 
 
 class ChessDataset(Dataset):
-    self.X = X
-    self.y = y
+    
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
 
-def __len__(self):
-    return len(self.X)
+    def __len__(self):
+        return len(self.X)
 
-def __getitem__(self, idx):
-    return self.X[idx], self.y[idx]
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
